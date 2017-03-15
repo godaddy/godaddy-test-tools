b4c8600792442e1071c02051c4d587be0374dafe
Sam Shull <sam.github@samshull.com>
2017-03-15 13:51:28 -0700

    Use latest godaddy-style, deprecate old tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,11 @@
 'use strict';
 
-require('./lib/index.js')(require('gulp'));
+require('./lib/index.js')(require('gulp'), {
+  lint: {
+    eslint: {
+      rules: {
+        strict: 0
+      }
+    }
+  }
+});
